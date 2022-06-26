21c27214cf94c82855544696e540cd7d3df23510
jolson2811 <jolson2811@gmail.com>
2022-06-25 18:18:48 -0600

    created uploadTransaction to post once online again

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,6 +18,7 @@ request.onsuccess = function (event) {
 
   // check if app is online,
   if (navigator.onLine) {
+    uploadTransaction();
   }
 };
 
@@ -37,3 +38,49 @@ function saveRecord(record) {
   // add record to your store with add method
   transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  // access your object store
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+
+  // get all records from store and set to a variable
+  const getAll = transactionObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          // access the new_pizza object store
+          const transactionObjectStore =
+            transaction.objectStore("new_transaction");
+          // clear all items in your store
+          transactionObjectStore.clear();
+
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+// listen for app coming back online
+window.addEventListener("online", uploadTransaction);
